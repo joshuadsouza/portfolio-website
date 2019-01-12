30d3f450bf9310255e149d745c4405a248f181a5
Joshua D'Souza <joshua@fastapps.co>
2019-01-12 15:42:17 -0500

    Adding Pages

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 const express = require("express");
 const app = express();
 app.set('view engine', 'ejs');
+app.use(express.static('public'))
 
 app.get('/', function(req, res){
     res.render('index');
+})
+
+app.get('/peaking-kilimanjaro', function(req, res){
+    res.render('kilimanjaro');
+})
+
+app.get('/elliott-hulse-transformation', function(req, res){
+    res.render('transformation');
+})
+
+app.get('/my-experience', function(req, res){
+    res.render('experience');
 })
 
 app.listen(3000, function(){
